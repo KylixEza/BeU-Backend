bc097d8a2b8bd57429c02aa5fbc412674ab4a99c
ACER <loilyxtra@gmail.com>
2023-02-13 20:13:15 +0700

    added several dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,10 @@ val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
 val h2_version: String by project
+val postgresql_version: String by project
+val hikari_version: String by project
+val koin_version: String by project
+val jnanoid_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.10"
@@ -29,6 +33,7 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-locations-jvm:$ktor_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("com.h2database:h2:$h2_version")
@@ -38,4 +43,21 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    //Postgresql
+    implementation("org.postgresql:postgresql:$postgresql_version")
+
+    //DI
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+
+    //Util
+    implementation("com.aventrix.jnanoid:jnanoid:$jnanoid_version")
+
+    //Hikari
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+
+    //Firebase
+    implementation("org.slf4j:slf4j-simple:2.0.5")
+    implementation("com.google.firebase:firebase-admin:9.1.1")
 }
